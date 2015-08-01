ba9551d9c52ac56e0ff84711a81121c0fe51a72e
nicktikhonov <nt34@st-andrews.ac.uk>
2015-08-01 11:56:01 +0100

    Add parsing of date

--- gitmermaid.py
+++ gitmermaid.py
@@ -7,19 +7,19 @@ args = parser.parse_args()
 
 cpmap = dict()
 
-command = "git log --pretty=%P%n%H%n%s --abbrev-commit --all"
+command = "git log --pretty=%p%n%h%n%s%n%cd --abbrev-commit --all"
 process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
 output = process.communicate()[0]
 
 lines = output.split('\n')
 
-groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
+groups = [lines[i:i+4] for i in range(0, len(lines), 4)]
 
 with open(args.name, 'w') as target:
     target.write("graph TD;\n")
 
     for group in groups:
-        if len(group) < 3:
+        if len(group) < 4:
             continue
 
         if group[0] == "":
@@ -30,6 +30,6 @@ with open(args.name, 'w') as target:
         message = group[2]
 
         for parent in parents:
-            target.write("{}[{}] --> {}\n".format(ident[:7], message,
-                parent[:7]))
+            target.write("{}[{}] --> {}\n".format(ident, message,
+                parent))
 
